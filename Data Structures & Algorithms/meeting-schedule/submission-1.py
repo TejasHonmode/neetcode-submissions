31106124f4611c2f681471a0fd7968bd84681204
TejasHonmode <tejashonmode98@gmail.com>
2026-04-23 19:45:11 -0500

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) == 1:
+            return True
+
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(1, len(intervals)):
+            i1 = intervals[i-1]
+            i2 = intervals[i]
+
+            if i2.start >= i1.end:
+                continue
+            else:
+                return False
+        return True
+
