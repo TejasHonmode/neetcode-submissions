fc9eeec915db0c259843c4816b25e5aea34eb4b7
TejasHonmode <tejashonmode98@gmail.com>
2026-04-25 01:30:49 -0500

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        c2o = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in c2o:
+                if stack and stack[-1] == c2o[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return len(stack) == 0
+        
